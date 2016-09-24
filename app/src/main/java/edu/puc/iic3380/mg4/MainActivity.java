94903ea19579ba4bcec706831bccb7c2f3266f67
Luiz Quelves da Silva <luiz.quelves@gmail.com>
2016-09-24 19:38:39 -0300

    Crear Project

---
+++ app/src/main/java/edu/puc/iic3380/mg4/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.puc.iic3380.mg4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
