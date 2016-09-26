a2ac15f23d9f478d674dd4dfb8038858d5a187de
Luiz Quelves da Silva <luiz.quelves@gmail.com>
2016-09-26 08:49:22 -0300

    Toolbar

---
+++ app/src/main/java/edu/puc/iic3380/mg4/activities/ControlCenterActivity.java
@@ -0,0 +1,31 @@
+package edu.puc.iic3380.mg4.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.puc.iic3380.mg4.R;
+
+public class ControlCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_center);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
